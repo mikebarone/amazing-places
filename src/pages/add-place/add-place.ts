7f194018386b234d056aba1aaaa9230f698e1132
mikebarone <mikebarone@gmail.com>
2017-06-27 21:46:10 -0400

    Select Location

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -14,7 +14,8 @@ export class AddPlacePage {
   location: Location = {
     lat: 40.7624324,
     lng: -73.9759827
-  }
+  };
+  locationIsSet = false;
 
   constructor(private  modalCtrl: ModalController) {}
 
@@ -24,9 +25,18 @@ export class AddPlacePage {
 
   onOpenMap() {
     const modal = this.modalCtrl.create(SetLocationPage, {
-      location: this.location
+      location: this.location,
+      isSet: this.locationIsSet
     });
     modal.present();
+    modal.onDidDismiss(
+      data => {
+        if(data) {
+          this.location = data.location;
+          this.locationIsSet = true;
+        }
+      }
+    );
   }
 
 }
--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavParams} from 'ionic-angular';
+import {IonicPage, NavParams, ViewController} from 'ionic-angular';
 import {Location} from "../../models/location"
 
 @IonicPage()
@@ -10,8 +10,26 @@ import {Location} from "../../models/location"
 export class SetLocationPage {
 
   location: Location;
+  marker: Location;
 
-  constructor(private navParams: NavParams) {
-    this.location = this.navParams.get('location');
+  constructor(
+    private navParams: NavParams,
+    public viewCtrl: ViewController) {
+      this.location = this.navParams.get('location');
+      if(this.navParams.get('isSet')) {
+        this.marker = this.location;
+      }
+  }
+
+  onSetMarker(event: any) {
+    this.marker = new Location(event.coords.lat, event.coords.lng);
+  }
+
+  onConfirm() {
+    this.viewCtrl.dismiss({location: this.marker});
+  }
+
+  onAbort() {
+    this.viewCtrl.dismiss();
   }
 }
